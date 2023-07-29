d00a064a4c4f4ac21e1dd469772dcafccab30e72
Cenaww <140489997+Cenaww@users.noreply.github.com>
2023-07-29 13:37:06 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201507559336'] //ur owner number
+global.ownername = "yasoo " //ur owner name%
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201507559336" //ur number
+global.premium = ['201507559336'] //ur premium number
+global.botname = 'DEKU' //ur bot name
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "DEKU" //ur sticker watermark packname
+global.author = "⍣01507559336⍣" //ur sticker watermark author
+global.wm = "DEKU." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -50,7 +45,6 @@ global.mess = {
     bot: '『 الميزه دي للبوت بس! 』',
     wait: '『 استني شويه... 』',
     linkm: '『 فين الرابط? 』',
-    endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
 }
 
